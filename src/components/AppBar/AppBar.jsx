cdc9d21842da89726bb17591ecb1082dd41b97f4
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-01-24 23:28:49 -0800

    app styles

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,41 +1,48 @@
-
 import UserNav from 'components/UserNav/UserNav';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { selectIsLoading, selectIsLoggedIn} from 'some/auth/auth-selectors';
+import { selectIsLoading, selectIsLoggedIn } from 'some/auth/auth-selectors';
 import css from './AppBar.module.css';
 import { Loader } from 'components/Loader/Loader';
 
 const AppBar = () => {
-  
   const userLoggedIn = useSelector(selectIsLoggedIn);
   const isLoading = useSelector(selectIsLoading);
 
   return (
     <header>
-      {isLoading && <Loader/>}
+      {isLoading && <Loader />}
+<div className={css.appContainer}>
+      <nav className={css.appNav}>
 
-      <div  className={css.appContainer}>
-              <nav className={css.appNav}>
-          <div>
-                     
-                      <NavLink to="/" className={css.navLink}>Home</NavLink>
+        <div>
+        <NavLink to="/" className={css.navLink}>
+          Home
+        </NavLink>
 
-            {userLoggedIn && <NavLink className={css.navLink} to="/contacts">Contacts</NavLink>}
-                  </div>
-                  
-          <div className={css.optionalMenu}>
-            {userLoggedIn ? (
-              <UserNav />
-            ) : (
-              <>
-                <NavLink to="/register" className={css.navLink}>Register</NavLink>
-                <NavLink  className={css.navLink} to="/login">Log in</NavLink>
-              </>
-            )}
+        {userLoggedIn && (
+          <NavLink className={css.navLink} to="/contacts">
+            Contacts
+          </NavLink>
+        )}
+        </div>
+        
+        <div>
+        {userLoggedIn ? (
+          <UserNav />
+        ) : (
+          <>
+            <NavLink to="/register" className={css.navLink}>
+              Register
+            </NavLink>
+            <NavLink className={css.navLink} to="/login">
+              Log in
+            </NavLink>
+          </>
+          )}
           </div>
         </nav>
-      </div>
+        </div>
     </header>
   );
 };
