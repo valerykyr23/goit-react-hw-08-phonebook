c26fc4c112d55cfa0e98e4e3a37e24d187f23734
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-01-26 19:15:46 -0800

    app styles and decor

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,10 +8,11 @@ const Home = () => {
         <div className={css.wrapper}>
             {/* <h1 className={css.title}>People.Relationships.Actions</h1> */}
             <p className={css.text}>Welcome to your personal PhoneBook !</p>
-            <img  src="https://thumbs.dreamstime.com/b/two-people-man-woman-character-talking-smart-phone-online-communication-email-message-concept-video-call-business-chat-242889380.jpg" alt="" width="780" height="780"></img>
+            <img  src="https://img.freepik.com/premium-vector/vector-illustration-person-carrying-mobile-phone-business-concept_492281-3979.jpg" alt="" width="700" height="700"></img>
 </div>
 //  https://www.shutterstock.com/image-vector/two-girls-talk-on-phone-600nw-1952146495.jpg
 // src="https://img.freepik.com/free-vector/get-touch-concept-illustration_114360-2586.jpg"
+        // src="https://thumbs.dreamstime.com/b/two-people-man-woman-character-talking-smart-phone-online-communication-email-message-concept-video-call-business-chat-242889380.jpg"
        
     )
 };
