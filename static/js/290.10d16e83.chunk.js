e556928899d6cf906d73c8202d459fb24ca6059d
valerykyr23 <95191772+valerykyr23@users.noreply.github.com>
2024-01-27 05:04:27 +0000

    Deploying to gh-pages from @ valerykyr23/goit-react-hw-08-phonebook@091305493b61c1b942bce81b5c1696aac853b409 🚀

---
+++ static/js/290.10d16e83.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[290],{7290:function(e,r,t){t.r(r),t.d(r,{default:function(){return c}});var o="Home_wrapper__iZIsl",a="Home_text__YlQ2v",n=t(184),c=function(){return(0,n.jsx)("div",{className:o,children:(0,n.jsx)("p",{className:a,children:"Welcome to your personal PhoneBook !"})})}}}]);
+//# sourceMappingURL=290.10d16e83.chunk.js.map
