9a0f4dbc641220ee72a1423377b7e29d8575f8bf
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-01-26 22:06:39 -0800

    app decor

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,12 @@ const Home = () => {
   return (
     <div className={css.wrapper}>
       <p className={css.text}>Welcome to your personal PhoneBook !</p>
-      {/* <img
+      <img
         src="https://media.istockphoto.com/id/1150386831/vector/people-friends-man-and-woman-chatting-talking-by-smartphone-internet-online-communication.jpg?s=612x612&w=0&k=20&c=cKkVqY7e0WkKE_XHoWC32-bvuPgh0TK7TALmBANsTtM="
         alt=""
         width="700"
         height="700"
-      ></img> */}
+      ></img>
     </div>
   );
 };
